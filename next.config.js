e39607c42a5f9aed272dc8a5358e6d068170b207
Luka Masanovic <lukamasanovic@gmail.com>
2021-10-03 18:57:14 +0200

    added map

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ['links.papareact.com']
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1Ijoid2hpenAiLCJhIjoiY2t1YXpoaGE1MGxuazMxbW81Y201bGVzbyJ9.w1iF7U2LJ3te3AhuPWCOvg'
     }
 }
--- pages/search.js
+++ pages/search.js
@@ -4,6 +4,8 @@ import Footer from '../components/Footer'
 import {format} from "date-fns";
 import {useRouter} from 'next/dist/client/router';
 import InfoCard from '../components/InfoCard';
+import Map from '../components/Map';
+
 
 
 function Search({searchResults}) {
@@ -45,6 +47,9 @@ function Search({searchResults}) {
                     ))}
                     </div>
 
+                </section>
+                <section className="hidden xl:inline-flex xl:min-w-[600px] ">
+                    <Map searchResults={searchResults}/>
                 </section>
 
 
